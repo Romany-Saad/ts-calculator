38801dee3052873c91f54ce8fada1b556d82d429
habib-beshara <habib.beshara4@gmail.com>
2021-08-12 11:57:23 +0200

    division by zero

--- __tests__/sample.test.ts
+++ __tests__/sample.test.ts
@@ -4,7 +4,25 @@ describe('calculator', () => {
     test("it can sum two numbers", () => {
         const c = new Calculator();
         expect(c.calc("2 + 2")).toEqual(4)
-
         expect(c.calc("2 + 3")).toEqual(5)
+    })
+
+    test("it can subtract two numbers", () => {
+        const c = new Calculator();
+        expect(c.calc("2 - 2")).toEqual(0)
+        expect(c.calc("2 - 3")).toEqual(-1)
+    })
+
+    test("it can multiply two numbers", () => {
+        const c = new Calculator();
+        expect(c.calc("2 * 2")).toEqual(4)
+        expect(c.calc("2 * 3")).toEqual(6)
+    })
+
+    test("it can divide two numbers", () => {
+        const c = new Calculator();
+        expect(c.calc("2 / 2")).toEqual(1)
+        expect(c.calc("2 / 3")).toEqual(2/3)
+        expect(c.calc("2 / 0")).toEqual(undefined)
     })
 })
--- src/Calculator.ts
+++ src/Calculator.ts
@@ -1,6 +1,9 @@
 export default class Calculator {
 
     public calc(s: string) {
+        const splitStr = s.split('/')
+        const divisionByZero = splitStr.some(char => parseFloat(char) === 0)
+        if (divisionByZero) return undefined
         return eval(s);
     }
 }
